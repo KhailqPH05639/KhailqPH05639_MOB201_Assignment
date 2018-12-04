5e0ae5c73dfc5187e5421a210409bdeefc61b602
KhailqPH05639 <khailqph05639@fpt.edu.vn>
2018-12-04 13:33:43 +0700

    update new

--- app/src/main/java/com/khai/khailqph05639_mob201_assignment/activity/MapsActivity.java
+++ app/src/main/java/com/khai/khailqph05639_mob201_assignment/activity/MapsActivity.java
@@ -8,16 +8,20 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,44 +30,49 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.khai.khailqph05639_mob201_assignment.R;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     LocationManager locationManager;
-
-    private GoogleApiClient client;
-
-
     private GoogleMap mMap;
+
+    public static final int RequestPermissionCode = 1;
+    protected GoogleApiClient googleApiClient;
+    protected double longitudeText;
+    protected double latitudeText;
+    protected Location lastLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+                .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
+                .addApi(LocationServices.API)
+                .build();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -82,11 +91,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+
     public void search_Sach(View view) {
         EditText edSearchSach = (EditText) findViewById(R.id.ed_search_Sach);
         String locationfined = edSearchSach.getText().toString();
         List<Address> addressList = null;
-        MarkerOptions markerOptions=new MarkerOptions();
+        MarkerOptions markerOptions = new MarkerOptions();
         if (locationfined.equalsIgnoreCase("")) {
             Toast.makeText(this, "Bạn chưa nhập địa chỉ", Toast.LENGTH_SHORT).show();
         } else {
@@ -98,11 +108,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
             for (int i = 0; i < addressList.size(); i++) {
                 Address myAddress = addressList.get(i);
-                LatLng latLng=new LatLng(myAddress.getLatitude(),myAddress.getLongitude());
+                LatLng latLng = new LatLng(myAddress.getLatitude(), myAddress.getLongitude());
                 markerOptions.position(latLng);
                 markerOptions.title(locationfined);
                 mMap.addMarker(markerOptions);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10f));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f));
             }
         }
 
@@ -111,109 +121,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     public void find_location(View view) {
-        Toast.makeText( this, "Dinh vi GPS", Toast.LENGTH_SHORT ).show();
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //
-            //                                      int[] grantResults)
-
-            buildGoogleApiClient();
-           mMap.setMyLocationEnabled(true);
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        // check the network provider is Enable
-        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    double latitude = location.getLatitude();
-                    double longtitude = location.getLongitude();
-                    LatLng latLng = new LatLng(latitude, longtitude);
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-                        List<Address> addressList = geocoder.getFromLocation(latitude, longtitude, 1);
-                        String str = addressList.get(0).getLocality() + ", ";
-                        str += addressList.get(0).getCountryName();
-                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-
-                }
-            });
-        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    double latitude = location.getLatitude();
-                    double longtitude = location.getLongitude();
-                    LatLng latLng = new LatLng(latitude, longtitude);
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-                        List<Address> addressList = geocoder.getFromLocation(latitude, longtitude, 1);
-                        String str = addressList.get(0).getLocality() + ", ";
-                        str += addressList.get(0).getCountryName();
-                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-
-                }
-            });
+        LatLng latLng = new LatLng(latitudeText, longitudeText);
+        Geocoder geocoder = new Geocoder(getApplicationContext());
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latitudeText, longitudeText, 1);
+            String str = addressList.get(0).getLocality() + ", ";
+            str += addressList.get(0).getCountryName();
+            mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    protected synchronized void buildGoogleApiClient() {
-        client = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks( this)
-                .addApi(LocationServices.API)
-                .build();
-        client.connect();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+        super.onStop();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermission();
+        } else {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                latitudeText = location.getLatitude();
+                                longitudeText = location.getLongitude();
+                                Toast.makeText(MapsActivity.this, latitudeText + "   " + longitudeText, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MapsActivity.this, new
+                String[]{ACCESS_FINE_LOCATION}, RequestPermissionCode);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("MainActivity", "Connection failed: " + connectionResult.getErrorCode());
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.e("MainActivity", "Connection suspendedd");
     }
+
 }
